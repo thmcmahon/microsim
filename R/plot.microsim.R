44e3ee837e508df74ef5bee82c69bf048dd958e8
Thomas McMahon <thmcmahon@gmail.com>
2018-01-12 10:58:35 +1100

    Added distributional percentage chart

--- R/plot.microsim.R
+++ R/plot.microsim.R
@@ -9,7 +9,7 @@
 #' @import ggplot2
 #' @import tidyr
 #' @import themeTom
-plot.microsim <- function(x, type = "average") {
+plot.microsim <- function(x, type = "average", base_title = "base", change_title = "change") {
   if (type == "average") {
     x$summary_tbl %>%
       select(income, base_avg, change_avg) %>%
@@ -21,21 +21,40 @@ plot.microsim <- function(x, type = "average") {
       scale_y_continuous(labels = scales::percent) +
       labs(x = "Income", y = "Average Tax Rate",
            title = "Average Tax Rates", subtitle = "Base vs Change") +
+      scale_colour_hue(labels = c(base_title, change_title)) +
       theme(legend.position = c(0.9, 0.2), legend.background = element_blank())
   } else if (type == "distribution") {
     x$distribution %>%
-      select(decile, revenue, avg_change) %>%
-      gather(category, value, -decile) %>%
-      mutate(category = recode(category, avg_change = "Average change ($)",
-                               revenue = "Revenue ($m)")) %>%
-      ggplot(aes(as.factor(decile), value)) + geom_col() +
-      facet_wrap(~ category) +
+      select(decile, avg_change) %>%
+      ggplot(aes(as.factor(decile), avg_change)) +
+      geom_col() +
       scale_y_continuous(labels = scales::dollar) +
       labs(x = "Taxable Income Decile", y = "",
-           title = "Distribution of impact of changes",
+           title = "Distribution of impact of changes in dollars",
+           subtitle = "Positive numbers mean increased tax, negative numbers mean a tax cut") +
+      theme_tom()
+  } else if (type == "distribution_share") {
+    x$distribution %>%
+      select(decile, avg_change_share) %>%
+      ggplot(aes(as.factor(decile), avg_change_share)) +
+      geom_col() +
+      scale_y_continuous(labels = scales::percent) +
+      labs(x = "Taxable Income Decile", y = "",
+           title = "Distribution of impact of changes as percentage of income",
            subtitle = "Positive numbers mean increased tax, negative numbers mean a tax cut") +
       theme_tom()
   } else {
-    stop("Type must either by `average` or `distribution`")
+    stop("Type must either by `average`, `distribution` or `distribution_share`")
   }
 }
+
+
+x$distribution %>%
+  select(decile, avg_change) %>%
+  ggplot(aes(as.factor(decile), avg_change)) +
+  geom_col() +
+  scale_y_continuous(labels = scales::dollar) +
+  labs(x = "Taxable Income Decile", y = "",
+       title = "Distribution of impact of changes",
+       subtitle = "Positive numbers mean increased tax, negative numbers mean a tax cut") +
+  theme_tom()
